47f13bebe52afd918356bec5bfe892fe9ea3f41a
anujathorat <thoratanuja2890@gmail.com>
2018-07-14 18:34:35 +0300

    Update dataservice.ts

--- src/app/_services/dataservice.ts
+++ src/app/_services/dataservice.ts
@@ -10,7 +10,7 @@ import {Observable} from 'rxjs/Observable';
 export class DataService {
   getCountries() {
     return [
-     new Country(1, 'USA' ),
+     new Country(1, 'Cyprus' ),
      new Country(2, 'India' ),
      new Country(3, 'Australia' )
     ];
@@ -29,10 +29,10 @@ export class DataService {
   
   getStates() {
    return [
-     new State(1, 1, 'Arizona' ),
-     new State(2, 1, 'Alaska' ),
-     new State(3, 1, 'Florida'),
-     new State(4, 1, 'Hawaii'),
+     new State(1, 1, 'Limassol' ),
+     new State(2, 1, 'Nicosia' ),
+     new State(3, 1, 'Larnaca'),
+     new State(4, 1, 'Paphos'),
      new State(5, 2, 'Gujarat' ),
      new State(6, 2, 'Goa'),
      new State(7, 2, 'Punjab' ),
